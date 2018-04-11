8ed891b96cebf39a8323f145ca967bf1800ed366
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-12 11:49:31 +1200

    datatable mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  transactions = [
+    {
+      person:'Gandalf Gray', title: 'Groceries', paymentDue: '12/04/2018', paymentDate: '12/04/2018', total: 104.34, type: 'DEPOSIT'
+    },
+    {
+      person:'Bilbo Baggins', title: 'Dark weed', paymentDue: '03/05/2018', paymentDate: '10/03/2018', total: 28.99, type: 'WITHDRAWAL'
+    },
+    {
+      person:'Frodo Baggins', title: 'Haircut', paymentDue: '08/04/2018', paymentDate: '08/04/2018', total: 45.00, type: 'DEPOSIT'
+    },
+  ];
+  
 }
