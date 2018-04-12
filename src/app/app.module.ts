fcd3beb0d739162ca4d74dd253e4fad01cc7736d
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-12 16:04:27 +1200

    people search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { TooltipModule } from 'primeng/tooltip'
 import { AppComponent } from './app.component';
 import { TransactionSearchComponent } from './transaction-search/transaction-search.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PersonSearchComponent } from './person-search/person-search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionSearchComponent,
-    NavBarComponent
+    NavBarComponent,
+    PersonSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/person-search/person-search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-search',
+  templateUrl: './person-search.component.html',
+  styleUrls: ['./person-search.component.css']
+})
+export class PersonSearchComponent {
+  
+  persons = [
+    {name: 'Gandalf Gray', city: 'Middle-Earth', suburb: 'Mordor', active: true },
+    {name: 'Frodo Baggins', city: 'Middle-Earth', suburb: 'Shire', active: false },
+    {name: 'Bilbo Baggins', city: 'Middle-Earth', suburb: 'Shire', active: true },
+  ];
+
+  cols = [
+    { field: 'name', header: 'Name' },
+    { field: 'city', header: 'City' },
+    { field: 'suburb', header: 'Suburb' },
+    { field: 'active', header: 'Active' },
+    { field: '', header: '' }
+];
+
+}
