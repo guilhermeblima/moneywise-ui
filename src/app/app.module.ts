6857a1eb6bd2ea7159690a0b671366c09ac651ae
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-12 13:09:19 +1200

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { TooltipModule } from 'primeng/tooltip'
 
 import { AppComponent } from './app.component';
 import { TransactionSearchComponent } from './transaction-search/transaction-search.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionSearchComponent
+    TransactionSearchComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
