7724b04100cfa4b59c392ecc8f5a0589eb53bc16
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-13 13:36:57 +1200

    pipes added

--- src/app/transaction-search/transaction-search.component.ts
+++ src/app/transaction-search/transaction-search.component.ts
@@ -9,13 +9,13 @@ export class TransactionSearchComponent {
 
   transactions = [
     {
-      person:'Gandalf Gray', title: 'Groceries', paymentDue: '12/04/2018', paymentDate: '12/04/2018', total: 104.34, type: 'DEPOSIT'
+      person:'Gandalf Gray', title: 'Groceries', paymentDue: new Date(2017,4,23), paymentDate: new Date(2017,4,23), total: 1104.34, type: 'DEPOSIT'
     },
     {
-      person:'Bilbo Baggins', title: 'Dark weed', paymentDue: '03/05/2018', paymentDate: '10/03/2018', total: 28.99, type: 'WITHDRAWAL'
+      person:'Bilbo Baggins', title: 'Dark weed', paymentDue:  new Date(2018,3,6), paymentDate: new Date(2017,4,23), total: 28.99, type: 'WITHDRAWAL'
     },
     {
-      person:'Frodo Baggins', title: 'Haircut', paymentDue: '08/04/2018', paymentDate: '08/04/2018', total: 45.00, type: 'DEPOSIT'
+      person:'Frodo Baggins', title: 'Haircut', paymentDue:  new Date(2018,3,21), paymentDate: new Date(2017,4,23), total: 45.00, type: 'DEPOSIT'
     },
   ];
 }
