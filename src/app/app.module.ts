4808c8188bcbcaf9f04e63015ed86862c37aa07b
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-16 16:04:04 +1200

    transaction form mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip'
-
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 import { AppComponent } from './app.component';
 import { TransactionSearchComponent } from './transaction-search/transaction-search.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PersonSearchComponent } from './person-search/person-search.component';
+import { TransactionFormComponent } from './transaction-form/transaction-form.component';
+
 
 
 @NgModule({
@@ -18,14 +21,17 @@ import { PersonSearchComponent } from './person-search/person-search.component';
     AppComponent,
     TransactionSearchComponent,
     NavBarComponent,
-    PersonSearchComponent
+    PersonSearchComponent,
+    TransactionFormComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
