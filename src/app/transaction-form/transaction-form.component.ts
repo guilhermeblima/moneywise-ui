2cecbd833a3a81a64d875d8c06c3b58a3948c855
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-16 19:52:58 +1200

    select button

--- src/app/transaction-form/transaction-form.component.ts
+++ src/app/transaction-form/transaction-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {SelectItem} from 'primeng/api';
 
 @Component({
   selector: 'app-transaction-form',
   templateUrl: './transaction-form.component.html',
   styleUrls: ['./transaction-form.component.css']
 })
-export class TransactionFormComponent implements OnInit {
+export class TransactionFormComponent {
 
-  constructor() { }
+  transactionTypes: SelectItem[];
 
-  ngOnInit() {
+  selectedType: string;
+
+  constructor(){
+    this.transactionTypes = [
+      {label: 'Deposit', value: 'DEPOSIT'}, 
+      {label: 'Withdrawal', value: 'WITHDRAWAL'}
+    ]
+
   }
-
 }
