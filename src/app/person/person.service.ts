965c64f0cbf7e0d7b06e86e0fe29a3afe7d0d27b
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-21 10:24:25 +1200

    person service

---
+++ src/app/person/person.service.ts
@@ -0,0 +1,38 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+
+export class PersonFilter{
+  name: string;
+  page = 0;
+  pageSize = 1;
+}
+
+@Injectable()
+export class PersonService {
+
+  constructor(private http: HttpClient) { }
+
+  urlResource = 'http://localhost:8080/persons';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic YWRtaW5AbW9uZXl3aXNlLmNvbTphZG1pbg=='
+    })
+  }
+
+  findAllSummary(filter: PersonFilter) {
+    let params = `&page=${filter.page}&size=${filter.pageSize}`;
+    if(filter.name){
+      params = `&name=${filter.name}`;
+    }
+    return this.http.get<any>(`${this.urlResource}?summary${params}`, this.httpOptions);
+  }
+
+  findAll() :Observable<any>{
+    return this.http.get(this.urlResource, this.httpOptions);
+  }
+
+}
