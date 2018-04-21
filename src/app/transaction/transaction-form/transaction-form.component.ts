a44a4ea4c395ebf3274e7f480942cd640d480c44
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-04-21 21:14:50 +1200

    loading categories transaction form

--- src/app/transaction/transaction-form/transaction-form.component.ts
+++ src/app/transaction/transaction-form/transaction-form.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+
 import {SelectItem} from 'primeng/api';
+
+import { CategoryService } from '../../category/category.service';
 
 @Component({
   selector: 'app-transaction-form',
   templateUrl: './transaction-form.component.html',
   styleUrls: ['./transaction-form.component.css']
 })
-export class TransactionFormComponent {
+export class TransactionFormComponent implements OnInit{
+
+  constructor(
+    private categoryService: CategoryService
+  ){}
 
   transactionTypes: SelectItem[];
 
@@ -14,25 +21,28 @@ export class TransactionFormComponent {
   selectedCategory: number;
   selectedPerson: number;
 
-  categories = [
-    {label: 'Groceries', value: 1},
-    {label: 'Haircut', value: 2},
-    {label: 'Medicine', value: 3},
-    {label: 'Power Bill', value: 4}
-  ]
+  categories = [];
 
   persons = [
     {label: 'Frodo Baggins', value: 1},
     {label: 'Bilbo Baggins', value: 2},
     {label: 'Gandalf Grey', value: 3},
     {label: 'Legolas Greenleaf', value: 4}
-  ]
+  ];
 
-  constructor(){
+  ngOnInit(){
     this.transactionTypes = [
       {label: 'Deposit', value: 'DEPOSIT'}, 
       {label: 'Withdrawal', value: 'WITHDRAWAL'}
     ]
-
+    this.loadCategories();
   }
+
+  loadCategories(){
+    this.categoryService.findAll()
+    .subscribe(response => {
+      this.categories = response.map(c =>({label: c.value, value: c.id}))
+    })
+  }
+
 }
